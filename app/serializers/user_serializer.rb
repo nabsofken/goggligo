7b9507453c689494732fe66723cfc7d75d4d292e
Arslan Rao <Arslanrao94@gmail.com>
2019-08-07 03:02:10 +0500

    User updated fileds

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :email, :created_at, :last_sign_in_at, :active, :business_name, :address, :phone, :fax, :website, :email, :contact_person_email, :contact_person_number
+  attributes :id, :first_name, :last_name, :email, :created_at, :last_sign_in_at, :active, :business_name, :address, :phone, :fax, :website, :email, :contact_person_email, :contact_person_number, :role
 end
