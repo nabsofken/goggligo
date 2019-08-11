3753b7915ecf5b5de1097fb8a19da37f36b24e6c
Arslan Rao <Arslanrao94@gmail.com>
2019-08-11 16:44:36 +0500

    Updated api call for template question

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::QuestionsController < ApiController
 
 
   def index
-    questions = current_user.questions
+    questions = current_user.questions + Question.template
     success(data: ActiveModelSerializers::SerializableResource.new(questions, each_serializer: QuestionSerializer, scope: {current_user: @current_user}))
   end
 end
