b008cb8b78099b6cc4bce3a1ea9c5bd0d9dcc6a8
Arslan Rao <Arslanrao94@gmail.com>
2019-09-01 03:00:27 +0500

    Add stats fixes

--- app/serializers/appointment_serializer.rb
+++ app/serializers/appointment_serializer.rb
@@ -2,13 +2,15 @@ class AppointmentSerializer < ActiveModel::Serializer
   attributes :id, :first_name, :last_name, :email, :mobile_number, :date_of_visit, :reason_of_visit, :answer_values
 
   def answer_values
-  	result = []
-  	self.object.answer_values.keys.each do |key|
-  		question = Question.find_by_id(key.to_s)
-  		next unless question.present?
-  		result << { id: question.id , title: question.title, value: self.object.answer_values[key] }
-  	end
-  	result
+    result = []
+    if self.object.answer_values.present?
+      self.object.answer_values.keys.each do |key|
+        question = Question.find_by_id(key.to_s)
+        next unless question.present?
+        result << { id: question.id , title: question.title, value: self.object.answer_values[key] }
+      end
+    end
+    result
   end
 
 end
