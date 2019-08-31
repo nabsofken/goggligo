ed99efdae5f6fb0ffeefa87a50929c9e63cef72f
Arslan Rao <Arslanrao94@gmail.com>
2019-09-01 02:35:20 +0500

    Add stats fixes

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -58,14 +58,15 @@ class QuestionsController < ApplicationController
     session[:notice] = nil
     @error = session[:error]
     session[:error] = nil
-    @questions = current_user.questions + Question.template if current_user.doctor?
+    @questions = Question.where("user_id = ? OR template = ?", current_user.id, true) if current_user.doctor?
+
     if current_user.admin?
       @questions = Question.all.order('created_at DESC') if params[:user_id].blank?
       user = User.find_by_id(params[:user_id])
       @questions = user.questions + Question.template if user.present?
     end
 
-    @questions = @questions.where('title LIKE ?', "%#{params[:search]}%") if params[:search].present?
+    @questions = @questions.where('lower(title) LIKE ?', "%#{params[:search].downcase}%") if params[:search].present?
     @questions = @questions.sort_by{|e| e[:id]}
   end
 
@@ -76,7 +77,7 @@ class QuestionsController < ApplicationController
     @error = session[:error]
     session[:error] = nil
     @questions = Question.template.order('created_at DESC')
-    @questions = @questions.where('title LIKE ?', "%#{params[:search]}%") if params[:search].present?
+    @questions = @questions.where('lower(title) LIKE ?', "%#{params[:search].downcase}%") if params[:search].present?
     @questions = @questions.sort_by{|e| e[:id]}
   end
 
