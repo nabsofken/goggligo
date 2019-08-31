6830c38cffc067f29435ba313454567f219cac08
Arslan Rao <Arslanrao94@gmail.com>
2019-09-01 02:04:07 +0500

    Sort question by id

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -4,6 +4,7 @@ class Api::V1::QuestionsController < ApiController
 
   def index
     questions = current_user.questions + Question.template
+    questions = questions.sort_by{|e| e[:id]}
     success(data: ActiveModelSerializers::SerializableResource.new(questions, each_serializer: QuestionSerializer, scope: {current_user: @current_user}))
   end
 end
