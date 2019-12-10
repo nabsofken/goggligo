1ea0214944e3ba709b2bbe5cf9e18a2180409819
Asnad Atta <asnad@zeptosystems.com>
2019-12-10 13:10:21 +0500

    use jquery rails

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,7 +5,7 @@
 // or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
 //
 //
-//= require jquery/jquery-2.1.1.js
+//= require jquery
 //= require bootstrap-sprockets
 //= require metisMenu/jquery.metisMenu.js
 //= require pace/pace.min.js
