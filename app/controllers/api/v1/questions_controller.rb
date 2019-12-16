c8ddc0b035c7011ee47fee4701642fddda9677c4
Asnad Atta <asnad@zeptosystems.com>
2019-12-16 22:07:57 +0500

    added scope for active

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::QuestionsController < ApiController
 
 
   def index
-    questions = current_user.questions + Question.template
+    questions = current_user.questions.active + Question.template.active
     questions = questions.sort_by{|e| [e.template? ? 0 : 1, e.id] }
     success(data: ActiveModelSerializers::SerializableResource.new(questions, each_serializer: QuestionSerializer, scope: {current_user: @current_user}))
   end
--- app/models/question.rb
+++ app/models/question.rb
@@ -4,7 +4,7 @@ class Question < ActiveRecord::Base
   enum question_type: { Text: 0, Number: 1, MCQs: 2, Date: 3, Gender: 4, InsuranceNumber: 5, MobileNumber: 6, Email: 7 }
   scope :template, -> { where(template: true) }
   scope :not_template, -> { where(template: false) }
-
+  scope :active, -> {where(active: true)}
   validates :title, presence: true
   validates :pre_condition_question_value, presence: true, if: "pre_condition_question_id.present?"
 
