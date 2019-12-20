efb7177d2e76592524708db3e76f269f64bb1d83
Asnad Atta <asnad@zeptosystems.com>
2019-12-20 12:25:34 +0500

    check email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,7 +13,6 @@ Myapp::Application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
@@ -83,6 +82,7 @@ Myapp::Application.configure do
   config.action_mailer.default_url_options = { host: ENV['APP_HOST'] }
   config.action_mailer.asset_host = ENV['APP_HOST']
 
+  config.action_mailer.raise_delivery_errors = true
   ActionMailer::Base.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
