0afcc0f2ecfc2301307de1fb5bc90ba98665f512
Asnad Atta <asnad@zeptosystems.com>
2019-12-30 17:41:08 +0500

    change redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,7 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :active, :email, :password, :password_confirmation, :device_id])
   end
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
 end
